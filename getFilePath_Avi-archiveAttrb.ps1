9b7b8622e6245a5ce1ae0505a4edb801613310f9
Pic0o <47042955+botsama@users.noreply.github.com>
2019-02-06 20:43:12 -0500

    Add files via upload

---
+++ getFilePath_Avi-archiveAttrb.ps1
@@ -0,0 +1,15 @@
+﻿# Get results of a folder, just output name and file sizes
+# This use case is for me when I do the podcasts. I need to put the file size into the XML for client side displaying.
+
+$boxName = $env:COMPUTERNAME
+$searchPath = 'H:\tCrowd_masters\fin\*'
+# Search path for all .avi files in and below target search folder.
+$buildSearchStr = Get-ChildItem $($searchPath) -Recurse -Force -Include *.avi -ErrorAction SilentlyContinue
+# This is an echo of direct outputted folder contents. Commented out since only for test
+#$buildSearchStr
+
+# Construct filtered output with only FileName and File Length outputting. 
+$filterOutputSet = $buildSearchStr | Select name, length | Sort name desc
+$filterOutputSet
+
+# I re-use previous scripts for various tasks. I find this helps me learn processes and coding in powershell, WAAAAY easier.
